d0fca7a4df000d9834e4823e572e7ed315168695
leanh1204 <anhlt.ados@gmail.com>
2021-08-24 21:35:51 +0700

    Create module Teen Number Checker

---
+++ Exercises7/src/com/leanh/Main.java
@@ -0,0 +1,26 @@
+package com.leanh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    public static boolean hasTeen(int age1, int age2, int age3) {
+        if(isTeen(age1) || isTeen(age2) || isTeen(age3)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen(int age) {
+        if((age >= 13 && age <= 19)){
+            return true;
+        }
+        return false;
+    }
+
+
+}
