0226066524a87438baea87efbb041415a0ecaba3
leanh1204 <anhlt.ados@gmail.com>
2021-08-24 16:30:04 +0700

    Create module Polymorphism

---
+++ OOP_Exercises39/src/com/leanh/Ford.java
@@ -0,0 +1,7 @@
+package com.leanh;
+
+public class Ford extends Car{
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+}
---
+++ OOP_Exercises39/src/com/leanh/Mitsubishi.java
@@ -0,0 +1,7 @@
+package com.leanh;
+
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+}
