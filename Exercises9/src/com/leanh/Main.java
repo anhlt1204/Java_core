e7e6b0cc40107c87c5b702fcbae519d607154f7a
leanh1204 <anhlt.ados@gmail.com>
2021-08-24 21:49:37 +0700

    Create module Minutes To Years and Days Calculator

---
+++ Exercises9/src/com/leanh/Main.java
@@ -0,0 +1,20 @@
+package com.leanh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long years = minutes / 525600;
+            long days = (minutes % 525600) / (24 * 60);
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
