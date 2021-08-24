948eed7f81818e377ee1b370f2a22c4b75af193c
leanh1204 <anhlt.ados@gmail.com>
2021-08-24 14:55:12 +0700

    Create module Cylinder

---
+++ OOP_Exercises35/src/com/leanh/Circle.java
@@ -0,0 +1,25 @@
+package com.leanh;
+
+public class Circle {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
